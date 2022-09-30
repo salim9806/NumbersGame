7b9f440be08a40a8f4f4dca8a9b226389feb4157
salim9806 <salim_abas@hotmail.com>
2022-09-30 06:23:09 +0200

    added difficulty level feature

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -6,12 +6,13 @@ namespace NumbersGame
     {
         static void Main(string[] args)
         {
-            byte attempts = 5;
-            Random random = new Random();
-            int randomNumber = random.Next(1, 20);
+            
             int guessedNumber;
             bool guessedRightNumber = false;
-            WriteLine("Välkommen!Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+            
+            SetDiffucultyLevel(out byte attempts, out int randomNumber, out (int min, int max) range);
+ 
+            WriteLine($"\nVälkommen!Jag tänker på ett nummer mellan {range.min} och {range.max}. \nKan du gissa vilket? Du får {attempts} försök.\n");
             
             while(attempts > 0)
             {
@@ -47,13 +48,55 @@ namespace NumbersGame
                 
         }
 
+        static void SetDiffucultyLevel(out byte attempts, out int randomNumber, out (int min, int max) range)
+        {
+            WriteLine("Välj din svårighetsgrad:\n\t1) Enkel nivå \n\t2) Millannivå \n\t3) Svår nivå");
+            switch (ReadLineIntWithinRange(1, 3))
+            {
+                case 1:
+                    attempts = 6;
+                    range = (1, 10);
+                    randomNumber = new Random().Next(1, 10);
+                    break;
+                case 2:
+                    attempts = 5;
+                    range = (1, 25);
+                    randomNumber = new Random().Next(1, 25);
+                    break;
+                case 3:
+                    attempts = 3;
+                    range = (1, 50);
+                    randomNumber = new Random().Next(1, 50);
+                    break;
+                default:
+                    attempts = 6;
+                    range = (1, 10);
+                    randomNumber = new Random().Next(1, 10);
+                    break;
+
+            }
+        }
+
         static int ReadLineInt()
         {
             int answer;
             
             while (!int.TryParse(ReadLine(), out answer))
             {
-                WriteLine("Du måste gissa ett nummer!");
+                WriteLine("Du måste ge ett nummer!");
+            }
+
+            return answer;
+        }
+
+        static int ReadLineIntWithinRange(int min, int max)
+        {
+            int answer = ReadLineInt();
+
+            while (answer < min || answer > max)
+            {
+                WriteLine($"Du måste ge ett nummer mellan {min} och {max}");
+                answer = ReadLineInt();
             }
 
             return answer;
